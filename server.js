b973303bd4a6b2cff105bb91e41464b70aa156ae
lpalleros <lucaspalleros@gmail.com>
2021-06-27 15:10:25 -0300

    socket io

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 
 const PORT = 4000;
 
-app.listen(PORT);
+http.listen(PORT);
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
+
+const messages = [
+    {name: 'lucas', message: 'simple message'},
+    {name: 'josh', message: 'simple messages'},
+]
+
+app.get('/messages', (req,res) => {
+    res.send(messages);
+});
+
+app.post('/messages', (req,res) => {
+    io.emit('message',req.body);
+    messages.push( req.body );
+    res.sendStatus(200);
+});
+
+io.on('connection', (socket) => {
+    console.log(socket);
+})
 
 app.get('/', (req,res) => {
     console.log('Connected');
